04e68cf947e0bc6eb85ed2496110d8a701328cca
Imma Valls <imma.valls@grafana.com>
2023-01-04 11:15:54 +0100

    adjust checks in 'sign should sign requests without host header'

--- tests/internal/signature.js
+++ tests/internal/signature.js
@@ -86,12 +86,13 @@ export function signatureV4TestSuite() {
                 })
 
                 expect(headers[AUTHORIZATION_HEADER]).to.equal(
-                    'AWS4-HMAC-SHA256 Credential=foo/20000101/us-bar-1/foo/aws4_request, SignedHeaders=host;x-amz-content-sha256;x-amz-date, Signature=36cfca5cdb2c8d094f100663925d408a9608908ffc10b83133e5b25829ef7f5f'
+                    'AWS4-HMAC-SHA256 Credential=foo/20000101/us-bar-1/foo/aws4_request, SignedHeaders=host;x-amz-content-sha256;x-amz-date, Signature=1e3b24fcfd7655c0c245d99ba7b6b5ca6174eab903ebfbda09ce457af062ad30'
                 )
-                expect(request).to.have.property(HOST_HEADER);
-                expect(request[HOST_HEADER]).to.not.be.undefined;
-                expect(request[HOST_HEADER]).to.not.be.null;
-                expect(request[HOST_HEADER]).to.not.be.empty;
+                
+                expect(headers).to.have.property(HOST_HEADER)
+                expect(headers[HOST_HEADER]).to.not.be.undefined
+                expect(headers[HOST_HEADER]).to.not.be.null
+                expect(headers[HOST_HEADER]).to.not.be.empty
             })
 
             describe('#sign should sign requests with string bodies', () => {
