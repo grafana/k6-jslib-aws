88edf566c18dcf390a8e86850ba58734e7cb3418
oleiade <theo@crevon.me>
2023-01-24 17:22:35 +0100

    Ensure the newly introduced SQS client creates properly named build files

--- examples/sqs.js
+++ examples/sqs.js
@@ -1,29 +1,29 @@
 import exec from 'k6/execution'
 
-import { AWSConfig, SQSClient } from '../build/sqs.min.js'
+import { AWSConfig, SQSClient } from '../build/sqs.js'
 
 const awsConfig = new AWSConfig({
     region: __ENV.AWS_REGION,
     accessKeyId: __ENV.AWS_ACCESS_KEY_ID,
     secretAccessKey: __ENV.AWS_SECRET_ACCESS_KEY,
     sessionToken: __ENV.AWS_SESSION_TOKEN,
-});
+})
 
-const sqs = new SQSClient(awsConfig);
-const testQueue = 'https://sqs.us-east-1.amazonaws.com/000000000/test-queue';
+const sqs = new SQSClient(awsConfig)
+const testQueue = 'https://sqs.us-east-1.amazonaws.com/000000000/test-queue'
 
-export default function() {
+export default function () {
     // If our test queue does not exist, abort the execution.
-    const queuesResponse = sqs.listQueues();
+    const queuesResponse = sqs.listQueues()
     if (queuesResponse.queueUrls.filter((q) => q === testQueue).length == 0) {
-        exec.test.abort();
+        exec.test.abort()
     }
 
     // Send message to test queue
     sqs.sendMessage({
         queueUrl: testQueue,
         messageBody: JSON.stringify({
-            value: '123'
-        })
-    });
+            value: '123',
+        }),
+    })
 }
